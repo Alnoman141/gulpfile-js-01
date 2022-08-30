65969abd68653954445d7f1a658705371546d556
Alnoman141 <nmnaba14@gmail.com>
2022-08-31 00:00:38 +0600

    browser sync added

--- gulpfile.js
+++ gulpfile.js
@@ -8,16 +8,40 @@ const babelify = require('babelify');
 const source = require('vinyl-source-stream');
 const buffer = require('vinyl-buffer');
 const uglify = require('gulp-uglify');
+var browserSync = require('browser-sync').create();
+var reload = browserSync.reload; // for auto reload
 
 var styleSRC = 'src/scss/style.scss';
 var styleDIST = './dist/css/';
 var styleWatch = 'src/scss/**/*.scss';
+var htmlWatch = '**/*.html';
 
 var jsSRC = 'script.js';
 var jsFOLDER = 'src/js/';
 var jsDIST = './dist/js/';
 var jsWatch = 'src/js/**/*.js';
 var jsFILES = [jsSRC];
+
+gulp.task('browser-sync', function(){
+    browserSync.init({
+        // for local environment
+        server: {
+            baseDir: './'
+        }
+
+        // for https
+        // open: false,
+        // injectChanges: true,
+        // proxy: 'https://gulp.dev',
+        // https: {
+        //     key: '',
+        //     cert: ''
+        // }
+        // we have to add <script async="" src="https://domain-name:port/browser-sync/browser-sync-client.js"></script>
+        // in index page as the last script
+    })
+})
+
 
 gulp.task('style', async function(){
     gulp.src(styleSRC)
@@ -32,7 +56,8 @@ gulp.task('style', async function(){
         }))
         .pipe(rename({suffix: '.min'}))
         .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest(styleDIST));
+        .pipe(gulp.dest(styleDIST))
+        .pipe(browserSync.stream()); // for auto reload
 })
 
 gulp.task('js', async function(){
@@ -49,12 +74,14 @@ gulp.task('js', async function(){
         .pipe(uglify())
         .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest(jsDIST))
+        .pause(browserSync.stream()); // for auto reload
     });
 })
 
 gulp.task('default', gulp.series('style', 'js'));
 
-gulp.task('watch', gulp.series('default'));
-gulp.watch(styleWatch, gulp.series('style'));
-gulp.watch(jsWatch, gulp.series('js'));
+gulp.task('watch', gulp.series('default','browser-sync'));
+gulp.watch(styleWatch, gulp.series('style', reload));
+gulp.watch(jsWatch, gulp.series('js', reload));
+gulp.watch(htmlWatch, reload);
 
